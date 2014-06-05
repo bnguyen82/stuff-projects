917e592d91ee45aaabd9c3e12278faac715c0ada
bnguyen82 <nguyenhuynthanhbao@gmail.com>
2014-06-05 23:19:24 +0700

    Add exercices

---
+++ AngularCourse/WebContent/exercises/day01/day01.js
@@ -0,0 +1,32 @@
+var products = [
+                {
+                	"name": "Midea Cooker", 
+                    "year": 2013, 
+                    "price": 100, 
+                    "producer": "Midea", 
+                }, 
+                {
+                	"name": "Iphone 5S", 
+                    "year": 2013, 
+                    "price": 500, 
+                    "producer": "Apple",                 
+                }, 
+                {
+                	"name": "Midea Cooker", 
+                    "year": 2013, 
+                    "price": 100, 
+                    "producer": "Midea",              
+                }, 
+                {
+                	"name": "Midea Cooker", 
+                    "year": 2013, 
+                    "price": 100, 
+                    "producer": "Midea", 
+                }, 
+                {
+                	"name": "Midea Cooker", 
+                    "year": 2013, 
+                    "price": 100, 
+                    "producer": "Midea", 
+                }
+            ];
