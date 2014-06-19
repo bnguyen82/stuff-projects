4faf3f6acf5a4893c8d2a8c1217e1047dd3f674c
bnguyen82 <nguyenhuynthanhbao@gmail.com>
2014-06-19 21:05:51 +0700

    Structrue folder

---
+++ phonegap/ionic/FirstApp/platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,12 @@
+D:\Works\Online\stuff-projects\phonegap\FirstApp\platforms\android\ant-build\classes.dex : \
+D:\Works\Online\stuff-projects\phonegap\FirstApp\platforms\android\ant-build\classes\com\ionic\keyboard\IonicKeyboard$1.class \
+D:\Works\Online\stuff-projects\phonegap\FirstApp\platforms\android\ant-build\classes\com\ionic\keyboard\IonicKeyboard.class \
+D:\Works\Online\stuff-projects\phonegap\FirstApp\platforms\android\ant-build\classes\com\ionicframework\starter\BuildConfig.class \
+D:\Works\Online\stuff-projects\phonegap\FirstApp\platforms\android\ant-build\classes\com\ionicframework\starter\HelloCordova.class \
+D:\Works\Online\stuff-projects\phonegap\FirstApp\platforms\android\ant-build\classes\com\ionicframework\starter\R$attr.class \
+D:\Works\Online\stuff-projects\phonegap\FirstApp\platforms\android\ant-build\classes\com\ionicframework\starter\R$drawable.class \
+D:\Works\Online\stuff-projects\phonegap\FirstApp\platforms\android\ant-build\classes\com\ionicframework\starter\R$string.class \
+D:\Works\Online\stuff-projects\phonegap\FirstApp\platforms\android\ant-build\classes\com\ionicframework\starter\R$xml.class \
+D:\Works\Online\stuff-projects\phonegap\FirstApp\platforms\android\ant-build\classes\com\ionicframework\starter\R.class \
+D:\Works\Online\stuff-projects\phonegap\FirstApp\platforms\android\ant-build\classes\org\apache\cordova\device\Device.class \
+D:\Works\Online\stuff-projects\phonegap\FirstApp\platforms\android\ant-build\dexedLibs\classes-4af51001a930efed1188c5accd61af6a.jar \
