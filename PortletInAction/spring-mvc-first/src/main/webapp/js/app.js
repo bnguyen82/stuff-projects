a8f426d737e326d246ce8ed695f96cdb84a15986
bnguyen82 <nguyenhuynthanhbao@gmail.com>
2014-06-28 12:49:56 +0700

    Update spring-mvc-first

--- PortletInAction/spring-mvc-first/src/main/webapp/js/app.js
+++ PortletInAction/spring-mvc-first/src/main/webapp/js/app.js
@@ -1,5 +1,6 @@
+/* MINIFIER: OFF */ 
 var module = angular.module('ngAppDemo', []);
 
 module.controller('ngAppDemoController', ['$scope', function($scope) {
-	$scope.a = "123456";
+	$scope.a = "1234567";
 }]); 
